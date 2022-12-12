efacbe883d87f84176c3a2346d5ea2248e0d3b62
brayan <br4y47.r4m0s@gmail.com>
2022-12-11 21:02:55 -0400

    Continuando la parte de detils

--- lib/screens/detils_screen.dart
+++ lib/screens/detils_screen.dart
@@ -10,20 +10,21 @@ class DetailsScreen extends StatelessWidget {
     //TODO cambiar luego por una instancia de movie
     final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
 
-    return const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar(),
+          const _CustomAppBar(),
           SliverList(
-            delegate: SliverChildDelegate(
-                
-            ),
+            delegate: SliverChildListDelegate([
+              const _PosterAndTitle()
+            ]),
           )
         ],
       )
     );
   }
 }
+
 
 class _CustomAppBar extends StatelessWidget {
   const _CustomAppBar({ Key? key }) : super(key: key);
@@ -60,3 +61,68 @@ class _CustomAppBar extends StatelessWidget {
     );
   }
 }
+
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder:  AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+
+            ),
+          ),
+          const SizedBox(width: 20,),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+
+              Text(
+                'movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(width: 5,),
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
